c9442bbf864db1a4cf88612f8fbdc0fbffe704bc
Daniel Forigua <forigua.daniel@gmail.com>
2026-05-24 16:44:06 -0500

    Se agrega migración inicial y configuración de base de datos

---
+++ InventarioAPI.Infrastructure/Migrations/20260524213938_CreacionInicial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InventarioAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreacionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Cantidad = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "Id", "Cantidad", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, 10, "Laptop" },
+                    { 2, 25, "Mouse" },
+                    { 3, 15, "Teclado" },
+                    { 4, 8, "Monitor" },
+                    { 5, 20, "Auriculares" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
